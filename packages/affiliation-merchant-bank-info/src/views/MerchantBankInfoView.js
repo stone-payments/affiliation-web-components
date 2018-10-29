2db20bcf777ba4010b1a3ce6cb96dce6b6df64c0
Caio Matheus Veloso Dias <cmdias@stone.com.br>
2018-10-29 17:17:36 -0300

    chore: :construction: Add merchant bank info views.

---
+++ packages/affiliation-merchant-bank-info/src/views/MerchantBankInfoView.js
@@ -0,0 +1,67 @@
+import { html } from 'sling-framework';
+import 'sling-web-component-message';
+import 'sling-web-component-loader';
+import 'sling-web-component-table';
+import 'sling-web-component-button';
+import { wrappedFromView } from './WrappedFormView.js';
+
+const columns = [{
+  title: 'Banco',
+  field: 'bankName',
+}, {
+  title: 'Tipo',
+  field: 'typeName',
+}, {
+  title: 'Agência',
+  field: 'agencyNumber',
+}, {
+  title: 'Dígito',
+  field: 'agencyNumberVerificationCode',
+}, {
+  title: 'Conta',
+  field: 'accountNumber',
+}, {
+  title: 'Dígito',
+  field: 'accountNumberVerificationCode',
+}];
+
+export const merchantBankInfoView = ({
+  state,
+  isLoading,
+  requestErrors,
+  addable,
+  editable,
+  handleStartEditing,
+  handleStopEditing,
+  handleFormSubmit,
+  handleFormUpdate,
+}) => html`
+  <style>
+    @import url('affiliation-merchant-bank-info/src/index.css');
+  </style>
+  <sling-message
+    aim="error"
+    srcdata="${requestErrors.length > 0 ? ['Ocorreu um erro ao acessar os dados.'] : []}"
+    layout="outline">
+  </sling-message>
+  ${wrappedFromView(state, handleStopEditing, handleFormSubmit, handleFormUpdate)}
+  <div class="business-component">
+    <sling-loader loading="${isLoading}"></sling-loader>
+    ${addable ? html`
+      <div class="business-component__button">
+        <sling-button
+          size="big"
+          color="success"
+          type="text">
+          adicionar dados bancários
+        </sling-button>
+      </div>
+    ` : ''}
+    <sling-table
+      editable="${editable}"
+      onrowclicked="${handleStartEditing}"
+      srcdata="${state.banks}"
+      srccolumns="${columns}">
+    </sling-table>
+  </div>
+  `;
---
+++ packages/affiliation-merchant-bank-info/src/views/RenderFormView.js
@@ -0,0 +1,98 @@
+import { html } from 'sling-framework';
+import { isNotEmpty, isValidBankId, isValidBankAgencyNumber, isValidBankAccountNumber } from 'sling-helpers';
+import 'sling-web-component-input';
+import 'sling-web-component-select';
+import 'sling-web-component-form';
+import { findAccountType } from '../state/MerchantbankInfoState.js';
+
+const validation = [
+  isNotEmpty('bankId'),
+  isValidBankId('bankId'),
+
+  isNotEmpty('typeId'),
+
+  isNotEmpty('agencyNumber'),
+  isValidBankAgencyNumber('agencyNumber'),
+
+  isNotEmpty('accountNumber'),
+  isValidBankAccountNumber('accountNumber'),
+
+  isNotEmpty('accountNumberVerificationCode'),
+];
+
+export const renderForm = (
+  state,
+  handleFormSubmit,
+  handleFormUpdate,
+) => {
+  const fields = state.formdata || {};
+  const accType = findAccountType(state.availableBanks || [], fields.bankId);
+
+  return html`
+    <sling-form
+      onformsubmit="${handleFormSubmit}"
+      onformupdate="${handleFormUpdate}"
+      validation=${validation}>
+      <sling-select
+        label="Banco"
+        name="bankId"
+        value="${fields.bankId}"
+        srcoptions="${state.availableBanks}">
+      </sling-select>
+        <sling-select
+        label="Tipo"
+        name="typeId"
+        value="${fields.typeId}"
+        srcoptions="${accType}">
+      </sling-select>
+      <sling-input
+        type="digits"
+        name="agencyNumber"
+        label="Agencia"
+        maxLength="5"
+        value="${fields.agencyNumber}">
+      </sling-input>
+      <sling-input
+        type="text"
+        name="agencyNumberVerificationCode"
+        label="Dígito"
+        maxLength="2"
+        value="${fields.agencyNumberVerificationCode}">
+      </sling-input>
+      <sling-input
+        type="digits"
+        name="accountNumber"
+        label="Conta"
+        maxLength="12"
+        value="${fields.accountNumber}">
+      </sling-input>
+      <sling-input
+        type="text"
+        name="accountNumberVerificationCode"
+        label="Dígito"
+        maxLength="2"
+        value="${fields.accountNumberVerificationCode}">
+      </sling-input>
+      <sling-input
+        type="hidden"
+        name="statusId"
+        value="${fields.statusId}">
+      </sling-input>
+      <sling-input
+        type="hidden"
+        name="centralizedPayment"
+        value="${fields.centralizedPayment}">
+      </sling-input>
+      <sling-input
+        type="hidden"
+        name="id"
+        value="${fields.id}">
+      </sling-input>
+      <sling-button
+        color="success"
+        type="submit">
+        Enviar
+      </sling-button>
+    </sling-form>
+  `;
+};
