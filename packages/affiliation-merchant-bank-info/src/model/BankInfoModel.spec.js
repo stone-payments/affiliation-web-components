43ec07195cfa3b93e6aa578d097bd8550c213690
Caio Matheus Veloso Dias <cmdias@stone.com.br>
2018-10-30 11:44:14 -0300

    chore: :white_check_mark: Update tests.

--- packages/affiliation-merchant-bank-info/src/model/BankInfoModel.spec.js
+++ packages/affiliation-merchant-bank-info/src/model/BankInfoModel.spec.js
@@ -1,44 +1,83 @@
-import { bankInfoModel } from './BankInfoModel.js';
+import { expect } from 'Chai';
+import {
+  bankInfoModel,
+  bankInfoFormResponseModel,
+} from './BankInfoModel.js';
 
-it('Should return correct Merchant Bank Info model.', () => {
-  const infoData = [
-    {
-      data: [{
-        id: 111,
-        compeCode: 111,
-        ispbCode: 11111,
-        name: 'test',
-        accountTypes: [
-          {
-            id: 2,
-            name: 'test',
-          },
-          {
-            id: 1,
-            name: 'test',
-          },
-        ],
-      }],
+const infoData = [
+  {
+    data: [{
+      id: 111,
+      compeCode: 111,
+      ispbCode: 11111,
+      name: 'test',
+      accountTypes: [
+        {
+          id: 2,
+          name: 'test',
+        },
+        {
+          id: 1,
+          name: 'test',
+        },
+      ],
+    }],
+  },
+  {
+    data: [{
+      affiliationCode: 111,
+      id: 111,
+      accountNumber: 2222,
+      accountNumberVerificationCode: 2,
+      agencyNumber: 1111,
+      agencyNumberVerificationCode: 2,
+      bankId: 4,
+      bankName: 'test',
+      statusId: 4,
+      typeId: 1,
+      typeName: 'test',
+      centralizedPayment: false,
+    }],
+  },
+];
+
+const stateFormValue = [
+  {
+    affiliationCode: '11111',
+    id: 1111,
+    accountNumber: '1111',
+    accountNumberVerificationCode: '1',
+    agencyNumber: '1',
+    agencyNumberVerificationCode: '1',
+    bankId: 111,
+    bankName: 'test',
+    statusId: 1,
+    typeId: 1,
+    typeName: 'test',
+    centralizedPayment: false,
+  },
+];
+
+const apiResponsesValues = [
+  {
+    data: {
+      bankId: 111,
+      accountNumber: '1111',
+      accountNumberVerificationCode: '1',
+      agencyNumber: '1111',
+      agencyNumberVerificationCode: '1',
+      centralizedPayment: false,
+      id: 111111,
+      statusId: 1,
+      typeId: 1,
     },
-    {
-      data: [{
-        affiliationCode: 111,
-        id: 111,
-        accountNumber: 2222,
-        accountNumberVerificationCode: 2,
-        agencyNumber: 1111,
-        agencyNumberVerificationCode: 2,
-        bankId: 4,
-        bankName: 'test',
-        statusId: 4,
-        typeId: 1,
-        typeName: 'test',
-        centralizedPayment: false,
-      }],
-    },
-  ];
-  expect(bankInfoModel(infoData))
-    .to.eql({
+    messages: [],
+  },
+];
+
+describe('Merchant Bank Info Model', () => {
+  it('Should return correct Merchant Bank Info model.', () => {
+    const modeledData = {
       banks: [{
         affiliationCode: 111,
         id: 111,
@@ -69,5 +108,30 @@ it('Should return correct Merchant Bank Info model.', () => {
           },
         ],
       }],
-    });
+    };
+
+    expect(bankInfoModel(infoData)).to.deep.equal(modeledData);
+  });
+
+  it('Should return correct Merchant Bank Info Form Response model', () => {
+    const modeledData = [
+      {
+        bankName: 'test',
+        typeName: 'test',
+        accountNumber: '1111',
+        accountNumberVerificationCode: '1',
+        agencyNumber: '1111',
+        agencyNumberVerificationCode: '1',
+        bankId: 111,
+        statusId: 1,
+        typeId: 1,
+        centralizedPayment: false,
+      },
+    ];
+
+    expect(bankInfoFormResponseModel(stateFormValue, apiResponsesValues))
+      .to
+      .deep
+      .equal(modeledData);
+  });
 });
