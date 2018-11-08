33da84f833c6aefb38fd409a7b6aea7cee496406
Caio Matheus Veloso Dias <cmdias@stone.com.br>
2018-11-08 18:14:32 -0200

    feat: :construction: Add affiliation-merchant-partners component.

---
+++ packages/affiliation-merchant-partners/src/component/MerchantPartners.js
@@ -0,0 +1,75 @@
+import { withRequest, withSetState } from 'sling-framework';
+import { v0 as sdk } from 'customer-js-sdk';
+import 'sling-web-component-table';
+import 'sling-web-component-loader';
+import 'sling-web-component-message';
+import 'sling-web-component-button';
+import { getMerchantPartnersView } from '../views/MerchantPartnersView';
+import { PartnersModel } from '../model/MerchantPartnersModel';
+
+const notEmpty = arg => arg != null;
+
+export const AffiliationMerchantPartners = (base = class {}) =>
+  class extends withRequest(withSetState(base)) {
+    constructor() {
+      super();
+
+      this.state = {
+        partners: [],
+      };
+    }
+
+    static get properties() {
+      return {
+        state: {
+          type: Object,
+          reflectToAttribute: false,
+        },
+        isLoading: {
+          type: Boolean,
+          reflectToAttribute: false,
+        },
+        requestErrors: {
+          type: Array,
+          reflectToAttribute: false,
+        },
+        editable: {
+          type: Boolean,
+          reflectToAttribute: true,
+        },
+        addable: {
+          type: Boolean,
+          reflectToAttribute: true,
+        },
+      };
+    }
+
+    static get requestParamNames() {
+      return ['affiliationCode'];
+    }
+
+    fetchData({ affiliationCode }) {
+      if (affiliationCode) {
+        sdk.headers.append('StoneCode', affiliationCode);
+        this
+          .request([
+            sdk.merchants.partners.get('https://portalapi.stone.com.br/',
+              { affiliationCode }),
+          ])
+          .then((responses) => {
+            if (responses.every(notEmpty)) {
+              const partners = PartnersModel(responses);
+              this.setState({ partners });
+            }
+          });
+      }
+    }
+
+    requestParamsChangedCallback(requestParams) {
+      this.fetchData(requestParams);
+    }
+
+    render() {
+      return getMerchantPartnersView(this);
+    }
+  };
