7a3bb8430cf5f9f06ebaae7e4d174f214a3f1ac0
kaio matheus <cmdias@stone.com.br>
2018-11-09 17:34:35 -0200

    chore: :white_check_mark: Update tests.

--- packages/affiliation-merchant-partners-list/src/component/MerchantPartnersList.test.js
+++ packages/affiliation-merchant-partners-list/src/component/MerchantPartnersList.test.js
@@ -1,6 +1,6 @@
 import { registerComponent } from 'sling-helpers';
 import { SlingElement } from 'sling-framework';
-import { AffiliationMerchantPartnersList } from './component/MerchantPartnersList.js';
+import { AffiliationMerchantPartnersList } from './MerchantPartnersList.js';
 
 registerComponent('affiliation-merchant-partners-list',
   AffiliationMerchantPartnersList(SlingElement));
