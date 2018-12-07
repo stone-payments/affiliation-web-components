3bad20fea6e55c99bc252c6e5bb1e7674f1563a7
Patrick Svaiter <psvaiter@stone.com.br>
2018-12-07 17:04:36 -0200

    chore: Fixes destructuring of address model.

--- packages/affiliation-merchant-addresses/src/model/MerchantAddressesModel.js
+++ packages/affiliation-merchant-addresses/src/model/MerchantAddressesModel.js
@@ -1,16 +1,14 @@
-export const AddressesModel = (data) => {
-  data.data.map(item => ({
-    key: item.key,
-    typeId: item.type.id,
-    typeName: item.type.name,
-    postalCode: item.postalCode,
-    stateCode: item.city.countrySubdivision.iso31662Short,
-    cityName: item.city.name,
-    street: item.streetName,
-    number: item.entranceNumber,
-    complement: item.complement,
-    neighborhood: item.neighborhood,
-    cityDisplay: `${item.city.name} / ${item.city.countrySubdivision.iso31662Short}`,
-    streetDisplay: `${item.streetName}, ${item.entranceNumber} ${item.complement}`,
-  }));
-};
+export const AddressesModel = ([data]) => data.map(item => ({
+  key: item.key,
+  typeId: item.type.id,
+  typeName: item.type.name,
+  postalCode: item.postalCode,
+  stateCode: item.city.countrySubdivision.iso31662Short,
+  cityName: item.city.name,
+  street: item.streetName,
+  number: item.entranceNumber,
+  complement: item.complement,
+  neighborhood: item.neighborhood,
+  cityDisplay: `${item.city.name} / ${item.city.countrySubdivision.iso31662Short}`,
+  streetDisplay: `${item.streetName}, ${item.entranceNumber} ${item.complement}`,
+}));
