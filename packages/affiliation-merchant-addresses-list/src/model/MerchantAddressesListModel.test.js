f09b71c15c17dac571e4253c9eca9c5a2ffa110c
Joao <jp-sh@live.com>
2019-01-02 17:24:13 -0200

    :hammer: Adjusts mock data.

--- packages/affiliation-merchant-addresses-list/src/model/MerchantAddressesListModel.test.js
+++ packages/affiliation-merchant-addresses-list/src/model/MerchantAddressesListModel.test.js
@@ -2,37 +2,35 @@ import { expect } from 'chai';
 import { AddressesListModel } from './MerchantAddressesListModel.js';
 
 const mockData = [
-  {
-    data: [
-      {
-        key: 'SOME-KEY',
-        entranceNumber: '15',
-        streetName: 'Test street name',
-        neighborhood: 'Test neighborhood',
-        postalCode: '11111-11',
-        complement: 'Test complement',
-        city: {
-          id: 1,
-          name: 'Test city name',
-          countrySubdivision: {
-            id: 'TS-MG',
-            name: 'Test country subdivision name',
-            iso31662Short: 'TS',
-            country: {
-              id: 1,
-              name: 'Test country',
-              iso31661Alpha3: 'TSA',
-              iso31661Alpha2: 'TAS',
-            },
+  [
+    {
+      key: 'SOME-KEY',
+      entranceNumber: '15',
+      streetName: 'Test street name',
+      neighborhood: 'Test neighborhood',
+      postalCode: '11111-11',
+      complement: 'Test complement',
+      city: {
+        id: 1,
+        name: 'Test city name',
+        countrySubdivision: {
+          id: 'TS-MG',
+          name: 'Test country subdivision name',
+          iso31662Short: 'TS',
+          country: {
+            id: 1,
+            name: 'Test country',
+            iso31661Alpha3: 'TSA',
+            iso31661Alpha2: 'TAS',
           },
         },
-        type: {
-          id: 1,
-          name: 'Test type name',
-        },
       },
-    ],
-  },
+      type: {
+        id: 1,
+        name: 'Test type name',
+      },
+    },
+  ],
 ];
 
 const expctedeData = [{
