9b61790225294e764c23f7e33f6f745ee6db03d2
Joao <jp-sh@live.com>
2019-01-02 17:15:12 -0200

    :hammer: Adjusts partners list model to the new affiliation v2 api contract.

--- packages/affiliation-merchant-partners-list/src/model/MerchantPartnersListModel.js
+++ packages/affiliation-merchant-partners-list/src/model/MerchantPartnersListModel.js
@@ -1,7 +1,20 @@
-export const PartnersListModel = ([data]) => data.map(item => ({
-  name: item.name,
-  email: item.email,
-  rg: item.rg,
-  cpf: item.cpf,
-}));
+export const PartnersListModel = ([data]) =>
+  data.reduce((allPartners, partner) => {
+    if (partner.naturalPerson) {
+      const modeledPartner = {
+        name: partner.naturalPerson.name,
+        documentType: partner.naturalPerson.taxIdType.name,
+        documentNumber: partner.naturalPerson.taxId,
+      };
+      allPartners.push(modeledPartner);
+    } else {
+      const modeledPartner = {
+        name: partner.legalPerson.name,
+        documentType: partner.legalPerson.taxIdType.name,
+        documentNumber: partner.legalPerson.taxId,
+      };
 
+      allPartners.push(modeledPartner);
+    }
+    return allPartners;
+  }, []);
