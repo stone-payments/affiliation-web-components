e46793f90c79e08314313331c86a99c7ccb4b483
Caio Matheus Veloso Dias <cmdias@stone.com.br>
2019-01-07 16:41:53 -0200

    chore: :construction: Update model data.

--- packages/affiliation-merchant-addresses-list/src/model/MerchantAddressesListModel.js
+++ packages/affiliation-merchant-addresses-list/src/model/MerchantAddressesListModel.js
@@ -2,13 +2,10 @@ export const AddressesListModel = ([
   data,
 ]) => {
   const modeledData = data.data.map(item => ({
-    type: item.type.name,
-    street: item.streetName,
-    number: item.entranceNumber,
-    complement: item.complement,
+    typeName: item.type.name,
+    streetDisplay: `${item.streetName}, ${item.entranceNumber} ${item.complement ? item.complement : ''}`,
     neighborhood: item.neighborhood,
-    city: item.city.name,
-    state: item.city.countrySubdivision.iso31662Short,
+    cityDisplay: `${item.city.name} / ${item.city.countrySubdivision.iso31662Short}`,
     postalCode: item.postalCode,
   }));
 
