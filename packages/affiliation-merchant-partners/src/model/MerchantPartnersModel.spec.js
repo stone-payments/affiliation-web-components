38272251cc4b0244f07eac3fcc535c4355eda0f2
Caio Matheus Veloso Dias <cmdias@stone.com.br>
2019-01-07 15:46:07 -0200

    chore: :white_check_mark: Update tests.

--- packages/affiliation-merchant-partners/src/model/MerchantPartnersModel.spec.js
+++ packages/affiliation-merchant-partners/src/model/MerchantPartnersModel.spec.js
@@ -1,31 +1,50 @@
 import { expect } from 'chai';
-import { PartnersModel } from './MerchantPartnersModel.js';
+import { NewPartnersModel } from './MerchantPartnersModel.js';
 
 describe('Merchant Partners Model', () => {
   it('Should return correct PartnerModel response', () => {
-    const dataResponse = [
+    const mockData = [
       [
         {
-          object: 'test',
-          id: '11111',
-          name: 'test',
-          email: 'test@test.com',
-          rg: 'N/I',
-          cpf: '111111111111',
-          birthday: '1111-11-11',
+          naturalPerson: {
+            name: 'TEST',
+            taxId: '1',
+            taxIdType: {
+              id: 1,
+              name: 'TEST',
+            },
+            ownershipPercentage: 1,
+            birthdate: '0000-00-07T00:00:00Z',
+            birthPlace: 'TEST',
+            birthCountry: {
+              id: 1,
+              name: 'TEST',
+              iso31661Alpha3: 'TEST',
+              iso31661Alpha2: 'TEST',
+            },
+            fatherName: 'TEST',
+            motherName: 'TEST',
+            spouseName: 'TEST',
+            spouseTaxId: 'TEST',
+            spouseTaxIdType: {
+              id: 1,
+              name: 'TEST',
+            },
+          },
         },
       ],
     ];
+
     const modeledData = [
       {
-        name: 'test',
-        email: 'test@test.com',
-        rg: 'N/I',
-        cpf: '111111111111',
+        name: 'TEST',
+        documentId: '1',
+        document: 'TEST',
+        partnerType: 'natural',
       },
     ];
 
-    expect(PartnersModel(dataResponse))
+    expect(NewPartnersModel(mockData))
       .to
       .deep
       .equal(modeledData);
