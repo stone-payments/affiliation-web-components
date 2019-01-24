0feff490dc2542d5125bafcf04ccdf3c17cfaf35
kaio matheus <cmdias@stone.com.br>
2019-01-24 16:10:15 -0200

    feat: :construction: Update views to new contract.

--- packages/affiliation-merchant-contacts/src/views/MerchantContactsView.js
+++ packages/affiliation-merchant-contacts/src/views/MerchantContactsView.js
@@ -3,7 +3,7 @@ import 'sling-web-component-table';
 import 'sling-web-component-loader';
 import 'sling-web-component-message';
 import 'sling-web-component-button';
-import { wrappedForm } from './WrappedFormView';
+import { getWrappedForm } from './WrappedFormView';
 
 const columns = [{
   title: 'Nome',
@@ -18,13 +18,9 @@ const columns = [{
   title: 'Telefone',
   field: 'phone',
   type: 'phone',
-}, {
-  title: 'Celular',
-  field: 'mobilePhone',
-  type: 'phone',
 }];
 
-export const merchantContactsView = ({
+export const getMerchantContactsView = ({
   state,
   addable,
   editable,
@@ -35,31 +31,31 @@ export const merchantContactsView = ({
   handleFormSubmit,
   handleFormUpdate,
 }) => html`
-  <style>
-     @import url('affiliation-merchant-contacts/src/index.css');
-    </style>
-    <sling-message
-      aim="error"
-      srcdata="${requestErrors.length > 0 ? ['Ocorreu um erro ao acessar os dados.'] : []}"
-      layout="outline"></sling-message>
-    ${wrappedForm(state, handleStopEditing, handleFormSubmit, handleFormUpdate)}
-    <div class="business-component">
-    <sling-loader loading="${isLoading}"></sling-loader>
-      ${addable ? html`
-        <div class="business-component__button">
-          <sling-button
-            size="big"
-            color="success"
-            type="text">
-            adicionar contato
-          </sling-button>
-        </div>
-      ` : ''}
-      <sling-table
-        editable="${editable}"
-        onrowclicked="${handleStartEditing}"
-        srcdata="${state.contacts}"
-        srccolumns="${columns}">
-      </sling-table>
-    </div>
-  `;
+    <style>
+      @import url('affiliation-merchant-contacts/src/index.css');
+      </style>
+      <sling-message
+        aim="error"
+        srcdata="${requestErrors.length > 0 ? ['Ocorreu um erro ao acessar os dados.'] : []}"
+        layout="outline"></sling-message>
+      ${getWrappedForm(state, handleStopEditing, handleFormSubmit, handleFormUpdate)}
+      <div class="business-component">
+      <sling-loader loading="${isLoading}"></sling-loader>
+        ${addable ? html`
+          <div class="business-component__button">
+            <sling-button
+              size="big"
+              color="success"
+              type="text">
+              adicionar contato
+            </sling-button>
+          </div>
+        ` : ''}
+        <sling-table
+          editable="${editable}"
+          onrowclicked="${handleStartEditing}"
+          srcdata="${state.contacts}"
+          srccolumns="${columns}">
+        </sling-table>
+      </div>
+    `;
