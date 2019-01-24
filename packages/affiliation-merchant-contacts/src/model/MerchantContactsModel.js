2f39f06b60dd2189f5f8e5e13bca317ddd40401e
kaio matheus <cmdias@stone.com.br>
2019-01-24 16:19:04 -0200

    feat: :construction: Update models to new contracts

--- packages/affiliation-merchant-contacts/src/model/MerchantContactsModel.js
+++ packages/affiliation-merchant-contacts/src/model/MerchantContactsModel.js
@@ -1,31 +1,28 @@
 import { replaceNonDigits } from 'sling-helpers';
 
-export const contactsModel = ([data]) => data.data;
+export const ContactsModel = contacts => contacts.map(contact => ({
+  name: contact.friendlyName,
+  typeName: contact.type.name,
+  typeId: contact.type.id,
+  email: contact.emails[0].email,
+  phone: `${contact.phones[0].areaCode}${contact.phones[0].phoneNumber}`,
+}));
 
-export const merchantContactsPayloadModel = (data) => {
-  const modeledData = {
-    name: data.name,
-    typeId: Number(data.typeId),
-    email: data.email,
-    phone: replaceNonDigits(data.phone),
-    mobilePhone: replaceNonDigits(data.mobilePhone),
-  };
+export const MerchantContactsPayloadModel = data => ({
+  name: data.name,
+  typeId: Number(data.typeId),
+  email: data.email,
+  phone: replaceNonDigits(data.phone),
+});
 
-  return modeledData;
-};
-
-export const updateContactstList = (list, [item]) => {
-  const data = list.map((el => (el.id === item.data.id
+export const UpdateContactstList = (list, [item]) =>
+  list.map((el => (el.id === item.data.id
     ? ({
       ...el,
       name: item.data.name,
       typeId: item.data.typeId,
       email: item.data.email,
       phone: item.data.phone,
-      mobilePhone: item.data.mobilePhone,
     })
     : el
   )));
-
-  return data;
-};
