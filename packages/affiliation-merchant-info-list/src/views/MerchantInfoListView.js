55dc2abb238cecbe3fb9ab9f6ff225b19a67e1dd
kaio matheus <cmdias@stone.com.br>
2019-02-21 18:30:02 -0300

    feat: :construction: Update view to new contract and add new feature, additional data.

--- packages/affiliation-merchant-info-list/src/views/MerchantInfoListView.js
+++ packages/affiliation-merchant-info-list/src/views/MerchantInfoListView.js
@@ -5,18 +5,32 @@ import 'sling-web-component-loader';
 import 'sling-web-component-message';
 import { getWrappedFormView } from './WrappedFormView.js';
 
-const keys = [
+
+const basicDataKeys = [
   'StoneCode',
-  'CNPJ',
+  'Documento',
   'Razão Social',
   'Nome Fantasia',
   'Ramo de Atividade',
+];
+
+const additionalDataKeys = [
+  'Documento adicional',
+  'Publicador',
+  'Data de publicação',
+  'Data de validade',
+  'Faturamento mensal estimado',
+  'Data de Nascimento',
+  'Local de nascimento',
+  'País de nascimento',
 ];
 
 export const getMerchantInfoListView = ({
   state,
   requestErrors,
   isLoading,
+  showAdditionalData,
+  showBasicData,
   cascadelist,
   editable,
   handleFormSubmit,
@@ -24,27 +38,42 @@ export const getMerchantInfoListView = ({
   handleStartEditing,
   handleStopEditing,
 }) => html`
-  <style>
-    @import url('affiliation-merchant-info-list/src/index.css');
-  </style>
-  <sling-message
-    aim="error"
-    srcdata="${requestErrors.length > 0 ? ['Ocorreu um erro ao acessar os dados.'] : []}"
-    layout="outline">
-  </sling-message>
-  <div class="business-component">
-  <sling-loader loading="${isLoading}"></sling-loader>
-  ${getWrappedFormView(state, handleStopEditing, handleFormSubmit, handleFormUpdate)}
-  <sling-list
-    cascadelist="${cascadelist}"
-    srcdata="${state.info}"
-    srckeys="${keys}">
-  </sling-list>
-  ${editable ? html`
-    <sling-button
-      onclick="${handleStartEditing}">
-      Editar
-    </sling-button>
-  ` : ''}
-  </div>
-`;
+    <style>
+      @import url('affiliation-merchant-info-list/src/index.css');
+    </style>
+    <sling-message
+      aim="error"
+      srcdata="${requestErrors.length > 0 ? ['Ocorreu um erro ao acessar os dados.'] : []}"
+      layout="outline">
+    </sling-message>
+    <div class="business-component">
+    <sling-loader loading="${isLoading}"></sling-loader>
+    ${getWrappedFormView(state, handleStopEditing, handleFormSubmit, handleFormUpdate)}
+    ${showBasicData
+    ? html`
+      <sling-list
+        cascadelist="${cascadelist}"
+        srcdata="${state.info.basicData}"
+        srckeys="${basicDataKeys}">
+      </sling-list>
+      `
+    : ''}
+    ${showAdditionalData
+    ? html`
+      <sling-list
+        cascadelist="${cascadelist}"
+        srcdata="${state.info.additionalData}"
+        srckeys="${additionalDataKeys}">
+      </sling-list>
+    `
+    : ''}
+    ${editable && showBasicData
+    ? html`
+      <sling-button
+        onclick="${handleStartEditing}">
+        Editar
+      </sling-button>
+    `
+    : ''}
+    </div>
+  `;
