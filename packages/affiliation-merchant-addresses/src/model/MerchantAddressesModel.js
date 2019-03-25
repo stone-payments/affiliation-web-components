10fba7cd67316575af8f85373b322c4a3f185d61
Caio Matheus Veloso Dias <cmdias@stone.com.br>
2019-03-25 18:34:08 -0300

    feat: Update addresses model, added payload and response models.

--- packages/affiliation-merchant-addresses/src/model/MerchantAddressesModel.js
+++ packages/affiliation-merchant-addresses/src/model/MerchantAddressesModel.js
@@ -1,26 +1,21 @@
-export const AddressesModel = ([{ data }]) => {
-  console.log('addresses model', data);
-
-  return data.map(item => ({
-    key: item.key,
-    typeId: item.type.id,
-    typeName: item.type.name,
-    postalCode: item.postalCode,
-    stateId: item.city.countrySubdivision.id,
-    stateCode: item.city.countrySubdivision.iso31662Short,
-    cityId: item.city.id,
-    cityName: item.city.name,
-    street: item.streetName,
-    number: item.entranceNumber,
-    complement: item.complement ? item.complement : 'N/A',
-    neighborhood: item.neighborhood,
-    cityDisplay: `${item.city.name} / ${item.city.countrySubdivision.iso31662Short}`,
-    streetDisplay: `${item.streetName}, ${item.entranceNumber} ${item.complement ? item.complement : 'N/A'}`,
-  }));
-};
+export const AddressesModel = ([{ data }]) => data.map(item => ({
+  key: item.key,
+  typeId: item.type.id,
+  typeName: item.type.name,
+  postalCode: item.postalCode,
+  stateId: item.city.countrySubdivision.id,
+  stateCode: item.city.countrySubdivision.iso31662Short,
+  cityId: item.city.id,
+  cityName: item.city.name,
+  street: item.streetName,
+  number: item.entranceNumber,
+  complement: item.complement ? item.complement : 'N/A',
+  neighborhood: item.neighborhood,
+  cityDisplay: `${item.city.name} / ${item.city.countrySubdivision.iso31662Short}`,
+  streetDisplay: `${item.streetName}, ${item.entranceNumber} ${item.complement ? item.complement : 'N/A'}`,
+}));
 
 export const StatesModel = ([, data]) => {
-  console.log('states model *******************', data);
   const modeledData = data.data.map(state => ({
     id: state.code,
     name: state.name,
@@ -30,11 +25,53 @@ export const StatesModel = ([, data]) => {
 };
 
 export const CitiesModel = (cities) => {
-  console.log('Cities model -----------------', cities.data);
   const modeledData = cities.data.map(citie => ({
     code: citie.name,
     name: citie.name,
   }));
 
   return modeledData;
+};
+
+export const PayloadModel = payload => ({
+  typeId: payload.typeId,
+  entranceNumber: payload.number,
+  streetName: payload.street,
+  neighborhood: payload.neighborhood,
+  postalCode: payload.postalCode,
+  cityName: payload.cityName,
+  countrySubdivisionCode: payload.stateCode,
+});
+
+export const AddressResponseModel = (currentState, responses) => {
+  const [data] = responses;
+  const modeledArray = [];
+
+  currentState.map((item) => {
+    if (item.key === data.key) {
+      const newEl = {
+        ...item,
+        key: item.key,
+        typeId: item.type.id,
+        typeName: item.type.name,
+        postalCode: item.postalCode,
+        stateId: item.city.countrySubdivision.id,
+        stateCode: item.city.countrySubdivision.iso31662Short,
+        cityId: item.city.id,
+        cityName: item.city.name,
+        street: item.streetName,
+        number: item.entranceNumber,
+        complement: item.complement ? item.complement : 'N/A',
+        neighborhood: item.neighborhood,
+        cityDisplay: `${item.city.name} / ${item.city.countrySubdivision.iso31662Short}`,
+        streetDisplay: `${item.streetName}, ${item.entranceNumber} ${item.complement ? item.complement : 'N/A'}`,
+      };
+
+      return modeledArray.push(newEl);
+    }
+
+    return modeledArray.push(item);
+  });
+
+  return modeledArray;
 };
