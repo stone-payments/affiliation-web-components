a14f1525a70e744539a5d154d1b12b40f2a795e4
Caio Matheus Veloso Dias <cmdias@stone.com.br>
2019-03-25 18:34:50 -0300

    feat: Update render form name.

--- packages/affiliation-merchant-addresses/src/view/WrappedFormView.js
+++ packages/affiliation-merchant-addresses/src/view/WrappedFormView.js
@@ -1,5 +1,5 @@
 import { html } from 'sling-framework';
-import { getRenderForm } from './RenderFormView.js';
+import { getMerchantAddressesRenderForm } from './RenderFormView.js';
 
 export const getWrappedFormView = (
   state,
@@ -19,7 +19,7 @@ export const getWrappedFormView = (
           <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
           <path d="M0 0h24v24H0z" fill="none"/>
       </svg>
-      ${getRenderForm(state, handleFormSubmit, handleFormUpdate)}
+      ${getMerchantAddressesRenderForm(state, handleFormSubmit, handleFormUpdate)}
     </div>
   </div>
   `;
