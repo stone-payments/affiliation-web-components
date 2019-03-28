fad53693649a784689794a4e77f4ff875e47764b
Caio Matheus Veloso Dias <cmdias@stone.com.br>
2019-03-28 18:45:00 -0300

    feat: Update tests.

--- packages/affiliation-merchant-addresses/src/model/MerchantAddressesModel.spec.js
+++ packages/affiliation-merchant-addresses/src/model/MerchantAddressesModel.spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { AddressesModel } from './MerchantAddressesModel.js';
+import { AddressesModel, StatesModel, PayloadModel } from './MerchantAddressesModel.js';
 
 const mockData = [
   {
@@ -32,26 +32,87 @@ const mockData = [
       },
     ],
   },
+  {
+    data: [
+      {
+        code: 'AC',
+        name: 'Acre',
+      }, {
+        code: 'RJ',
+        name: 'Rio de janeiro',
+      }, {
+        code: 'SP',
+        name: 'São Paulo',
+      },
+    ],
+  },
 ];
 
-const expctedeData = [{
-  key: 'test',
-  typeId: 1,
-  typeName: 'test',
-  postalCode: 'test',
-  stateCode: 'test',
-  cityName: 'test',
-  streetName: 'test',
-  number: 1,
-  complement: 'N/A',
-  neighborhood: 'test',
-  cityDisplay: 'test / test',
-  streetDisplay: 'test, 1 N/A',
-}];
-
 describe('Merchant Addresses Model', () => {
-  it('Should return a sucesses modeled data', () => {
+  it('Should return a sucesses AddressesModel modeled data', () => {
+    const expctedeData = [{
+      key: 'test',
+      typeId: 1,
+      typeName: 'test',
+      postalCode: 'test',
+      stateCode: 'test',
+      cityName: 'test',
+      streetName: 'test',
+      number: 1,
+      complement: 'N/A',
+      neighborhood: 'test',
+      cityDisplay: 'test / test',
+      streetDisplay: 'test, 1 N/A',
+    }];
+
     const testData = AddressesModel(mockData);
+
+    expect(testData).to.deep.equal(expctedeData);
+  });
+
+  it('Should return a sucesses StatesModel data', () => {
+    const expctedeData = [
+      {
+        id: 'AC',
+        name: 'Acre',
+      }, {
+        id: 'RJ',
+        name: 'Rio de janeiro',
+      }, {
+        id: 'SP',
+        name: 'São Paulo',
+      }];
+
+    const testData = StatesModel(mockData);
+
+    expect(testData).to.deep.equal(expctedeData);
+  });
+
+  it('Should return a sucesses PayloadModel data', () => {
+    const mockPayload = {
+      key: 'test',
+      typeId: 1,
+      typeName: 'test',
+      postalCode: 'test',
+      stateCode: 'test',
+      cityName: 'test',
+      streetName: 'test',
+      number: 1,
+      complement: 'N/A',
+      neighborhood: 'test',
+    };
+
+    const expctedeData = {
+      typeId: 1,
+      entranceNumber: 1,
+      streetName: 'test street name',
+      neighborhood: 'test',
+      postalCode: 'test',
+      cityName: 'test',
+      countrySubdivisionCode: 'test',
+    };
+
+    const testData = PayloadModel(mockPayload);
 
     expect(testData).to.deep.equal(expctedeData);
   });
