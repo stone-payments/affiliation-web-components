62f40070b805f5a4a9cbdbfaa3ff03e2186e2307
Caio Matheus Veloso Dias <cmdias@stone.com.br>
2019-04-12 12:54:24 -0300

    feat: Format MerchantContactsState.

--- packages/affiliation-merchant-contacts/src/state/MerchantContactsState.js
+++ packages/affiliation-merchant-contacts/src/state/MerchantContactsState.js
@@ -1,15 +1,14 @@
 export const contactsTypeId = [
-    {
-        id: 1,
-        name: 'Administrativo'
-    },
-    {
-        id: 2,
-        name: 'Financeiro'
-    },
-    {
-        id: 3,
-        name: 'Suporte Técnico'
-    }
-
-]
+  {
+    id: 1,
+    name: 'Administrativo',
+  },
+  {
+    id: 2,
+    name: 'Financeiro',
+  },
+  {
+    id: 3,
+    name: 'Suporte Técnico',
+  },
+];
