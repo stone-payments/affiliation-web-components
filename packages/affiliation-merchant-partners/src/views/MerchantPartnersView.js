7a1bd2490b3f98d32ca8d361796d9af6cce18022
caiomdias <caiomdias_@hotmail.com>
2019-04-16 04:17:05 -0300

    feat: Update MerchantPartnersView, add edite and create features.

--- packages/affiliation-merchant-partners/src/views/MerchantPartnersView.js
+++ packages/affiliation-merchant-partners/src/views/MerchantPartnersView.js
@@ -1,6 +1,8 @@
 import { html } from 'sling-framework';
 import '@stone-payments/emd-basic-table';
-import { getWrappedFormView } from './WrappedLegalForm.js';
+import { getWrappedLegalFormView } from './WrappedLegalForm.js';
+import { getWrappedNaturalFormView } from './WrappedNaturalForm.js';
+import { getWrappedCreatePartnerFormView } from './WrappedCreatePartnerForm.js';
 
 const LegalPersonColumns = [{
   title: 'Nome Fantasia',
@@ -10,7 +12,7 @@ const LegalPersonColumns = [{
   field: 'document',
 }, {
   title: 'N° documento',
-  field: 'documentId',
+  field: 'taxId',
   type: 'cnpj',
 }];
 
@@ -18,13 +20,37 @@ const naturalPrsonColumns = [{
   title: 'Nome',
   field: 'name',
 }, {
-  title: 'Tipo de documento',
-  field: 'document',
-}, {
-  title: 'N° documento',
-  field: 'documentId',
+  title: 'CPF',
+  field: 'taxId',
   type: 'CPF',
-}];
+}, {
+  title: 'Nome da Mãe',
+  field: 'motherName',
+}, {
+  title: 'Nascimento',
+  field: 'displayBirthDate',
+}, {
+  title: 'Cidade de nascimento',
+  field: 'birthPlace',
+}, {
+  title: 'RG',
+  field: 'additionalDocumentIdentifier',
+}, {
+  title: 'Emissão',
+  field: 'displayAdditionalDocumentIssueDate',
+}, {
+  title: 'Expedidor',
+  field: 'additionalDocumentIssuedBy',
+}, {
+  title: 'Renda mensal',
+  field: 'estimatedMonthlyBilling',
+}, {
+  title: 'Patrimônio',
+  field: 'patrimonio',
+},
+
+];
+
 
 export const getMerchantPartnersView = ({
   state,
@@ -34,11 +60,17 @@ export const getMerchantPartnersView = ({
   legalPersons,
   isLoading,
   requestErrors,
-  createPartner,
+  handleStartCreatePartner,
+  handleStopCreatePartner,
+  handleSubmitCreatePartner,
   handleStartEditeLegalPerson,
   handleStopEditeLegalPerson,
   handleSubmitEditeLegalPersonForm,
   handleUpdateEditeLegalPersonForm,
+  handleStartEditeNaturalPerson,
+  handleStopEditeNaturalPerson,
+  handleSubmitEditeNaturalPersonForm,
+  handleUpdateEditeNaturalPersonForm,
 }) => html`
     <style>
       @import url('affiliation-merchant-partners/src/index.css');
@@ -48,24 +80,16 @@ export const getMerchantPartnersView = ({
       srcdata="${requestErrors.length > 0 ? ['Ocorreu um erro ao acessar os dados.'] : []}"
       layout="outline">
     </sling-message>
-    ${getWrappedFormView(
+   
+    <div class="business-component">
+      <sling-loader loading="${isLoading}"></sling-loader>
+    
+    
+    ${getWrappedLegalFormView(
     state,
     handleStopEditeLegalPerson,
     handleSubmitEditeLegalPersonForm,
     handleUpdateEditeLegalPersonForm)}
-    <div class="business-component">
-      <sling-loader loading="${isLoading}"></sling-loader>
-      ${addable ? html`
-        <div class="business-component__button">
-          <sling-button
-            onclick="${createPartner}"
-            size="big"
-            color="success"
-            type="text">
-            adicionar contato
-          </sling-button>
-        </div>
-      ` : ''}
     ${legalPersons
     ? html`
     <h3>Legal Person</h3>
@@ -77,11 +101,32 @@ export const getMerchantPartnersView = ({
       </sling-table>
       `
     : ''}
+
+    ${addable ? html`
+    ${getWrappedCreatePartnerFormView(
+    state,
+    handleStopCreatePartner,
+    handleSubmitCreatePartner)}
+      <div class="business-component__button">
+        <sling-button
+          onclick="${handleStartCreatePartner}"
+          size="big"
+          color="success"
+          type="submit">
+          adicionar sócio Pessoa Física
+        </sling-button>
+      </div>
+    ` : ''}
+    ${getWrappedNaturalFormView(
+    state,
+    handleStopEditeNaturalPerson,
+    handleSubmitEditeNaturalPersonForm,
+    handleUpdateEditeNaturalPersonForm)}
     ${naturalPersons
     ? html`
     <h3>Neutral Person</h3>
       <sling-table
-        onrowclicked="${''}"
+        onrowclicked="${handleStartEditeNaturalPerson}"
         editable="${editable}"
         srcdata="${state.partners.naturalPersons}"
         srccolumns="${naturalPrsonColumns}">
