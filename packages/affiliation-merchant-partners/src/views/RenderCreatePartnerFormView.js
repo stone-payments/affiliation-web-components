4d5a5a144d87540362921c4b02ee2a24450cb397
caiomdias <caiomdias_@hotmail.com>
2019-04-16 04:17:24 -0300

    feat: Add RenderCreatePartnerFormView.

---
+++ packages/affiliation-merchant-partners/src/views/RenderCreatePartnerFormView.js
@@ -0,0 +1,117 @@
+import { html } from 'sling-framework';
+import { isNotEmpty } from 'sling-helpers';
+import 'sling-web-component-input';
+import 'sling-web-component-select';
+import 'sling-web-component-form';
+import {
+  documentTypeIds,
+  additionalDocumentTypeIds,
+  countryCodes,
+} from '../state/MerchantPartnersState.js';
+
+
+const validation = [
+  isNotEmpty('name'),
+  isNotEmpty('documentTypeId'),
+  isNotEmpty('taxId'),
+  isNotEmpty('birthDate'),
+  isNotEmpty('birthPlace'),
+  isNotEmpty('birthCountryid'),
+  isNotEmpty('additionalDocumentId'),
+  isNotEmpty('additionalDocumentIdentifier'),
+  isNotEmpty('additionalDocumentIssueDate'),
+  isNotEmpty('additionalDocumentIssuedBy'),
+  isNotEmpty('motherName'),
+];
+
+export const getRenderCreatePartnerForm = (
+  state,
+  handleSubmitCreatePartner,
+) => html`
+  <sling-form
+    onformsubmit="${handleSubmitCreatePartner}"
+    validation=${validation}>
+    <sling-input
+      type="text"
+      name="name"
+      label="Nome">
+    </sling-input>
+    <sling-select
+      label="Documento"
+      name="documentTypeId""
+      srcoptions="${documentTypeIds}">
+    </sling-select>
+    <sling-input
+      type="text"
+      name="taxId"
+      label="N° do documento">
+    </sling-input>
+    <sling-input
+      type="date"
+      name="birthDate"
+      label="Data de nascimento">
+    </sling-input>
+    <sling-input
+      type="text"
+      name="birthPlace"
+      label="Local de Nascimento">
+    </sling-input>
+    <sling-select
+      type="text"
+      name="birthCountryid"
+      label="País de Nascimento"
+      srcoptions="${countryCodes}">
+    </sling-select>
+    <sling-select
+      label="Documento Adicional"
+      name="additionalDocumentId""
+      srcoptions="${additionalDocumentTypeIds}">
+    </sling-select>
+    <sling-input
+      type="text"
+      name="additionalDocumentIdentifier"
+      label="N° do documento">
+    </sling-input>
+    <sling-input
+      type="date"
+      name="additionalDocumentIssueDate"
+      label="Data de expedição">
+    </sling-input>
+    <sling-input
+      type="text"
+      name="additionalDocumentIssuedBy"
+      label="Orgão expeditor">
+    </sling-input>
+    <sling-input
+      type="text"
+      name="fatherName"
+      label="Nome do pai">
+    </sling-input>
+    <sling-input
+      type="text"
+      name="motherName"
+      label="Nome da mãe">
+    </sling-input>
+    <sling-input
+      type="text"
+      name="spouseName"
+      label="Nome do cônjuge">
+    </sling-input>
+    <sling-select
+      label="Documento"
+      name="spouseTaxIdType""
+      srcoptions="${documentTypeIds}">
+    </sling-select>
+    <sling-input
+      type="text"
+      name="spouseTaxId"
+      label="N° do documento do cônjuge">
+    </sling-input>
+    <sling-button
+      color="success"
+      type="submit">
+      Enviar
+    </sling-button>
+  </sling-form>
+`;
+
