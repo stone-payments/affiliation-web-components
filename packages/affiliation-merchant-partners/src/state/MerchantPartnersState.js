ccb7fe40e70d537c0e5a4b14b279d04205398838
caiomdias <caiomdias_@hotmail.com>
2019-04-16 04:16:42 -0300

    feat: Add MerchantPartnersState.

---
+++ packages/affiliation-merchant-partners/src/state/MerchantPartnersState.js
@@ -0,0 +1,23 @@
+export const documentTypeIds = [
+  {
+    id: 2,
+    name: 'CPF',
+  },
+];
+
+export const additionalDocumentTypeIds = [
+  {
+    id: 1,
+    name: 'RG - Registro Geral',
+  },
+];
+
+export const countryCodes = [
+  {
+    id: 1,
+    name: 'test1',
+  }, {
+    id: 2,
+    name: 'test2',
+  },
+];
