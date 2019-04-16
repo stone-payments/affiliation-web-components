b320b4d17541d36ae7ce0a65d2151051a0426a3a
caiomdias <caiomdias_@hotmail.com>
2019-04-16 04:11:07 -0300

    feat: Update MerchantPartnersModel, add edite, create and payloads models.

--- packages/affiliation-merchant-partners/src/model/MerchantPartnersModel.js
+++ packages/affiliation-merchant-partners/src/model/MerchantPartnersModel.js
@@ -1,25 +1,78 @@
+export const formatDate = (date, target) => {
+  const tempDate = new Date(date);
+  let day = tempDate.getDate().toString();
+  let month = (tempDate.getMonth() + 1).toString();
+  const year = tempDate.getFullYear();
+
+  day = (day.length === 1) ? `0${day}` : day;
+  month = (month.length === 1) ? `0${month}` : month;
+
+  if (target === 'display') {
+    return `${day}/${month}/${year}`;
+  }
+
+  if (target === 'BR') {
+    return `${day}-${month}-${year}`;
+  }
+
+  return `${year}-${month}-${day}`;
+};
+
+const findByDocumentTypeId = data =>
+  data.find(item => item.documentTypeId === 1);
+
 export const PartnersModel = ([{ data }]) => {
   const modeledPartnersData =
   {
     naturalPersons: [],
     legalPersons: [],
   };
+  console.log('data', data);
 
   return data.reduce((allPartners, partner) => {
-    console.log(allPartners, partner);
     if (partner.naturalPerson) {
+      const additionalDocument =
+        findByDocumentTypeId(partner.naturalPerson.additionalDocuments);
+
       const modeledPartner = {
         name: partner.naturalPerson.name,
-        documentId: partner.naturalPerson.taxId,
+        motherName: partner.naturalPerson.motherName,
+        fatherName: partner.naturalPerson.fatherName,
+        spouseName: partner.naturalPerson.spouseName,
+        spouseTaxId: partner.naturalPerson.spouseTaxId,
+        spouseTaxIdType: partner.naturalPerson.spouseTaxIdType,
+        birthDate: formatDate(partner.naturalPerson.birthdate),
+        birthPlace: partner.naturalPerson.birthPlace,
+        birthCountryid: partner.naturalPerson.birthCountry.id,
+        taxId: partner.naturalPerson.taxId,
+        ownershipPercentage: partner.naturalPerson.ownershipPercentage,
         document: partner.naturalPerson.taxIdType.name,
+        documentTypeId: partner.naturalPerson.taxIdType.id,
+        additionalDocumentId: additionalDocument.documentTypeId,
+        additionalDocumentIdentifier: additionalDocument.documentIdentifier,
+        additionalDocumentIssueDate: formatDate(additionalDocument.issueDate),
+        additionalDocumentIssuedBy: additionalDocument.issuedBy,
+        estimatedMonthlyBilling:
+        partner.naturalPerson.estimatedMonthlyBilling || '',
         partnerType: 'natural',
+        // Mapear propiedade correta.
+        patrimonio: '',
+
+        displayAdditionalDocumentIssueDate:
+          formatDate(additionalDocument.issueDate, 'display'),
+        displayBirthDate:
+          formatDate(partner.naturalPerson.birthdate, 'display'),
       };
       allPartners.naturalPersons.push(modeledPartner);
     } else {
       const modeledPartner = {
         tradeName: partner.legalPerson.tradeName,
-        documentId: partner.legalPerson.taxId,
+        taxId: partner.legalPerson.taxId,
+        ownershipPercentage: partner.legalPerson.ownershipPercentage,
         document: partner.legalPerson.taxIdType.name,
+        documenttypeId: partner.legalPerson.taxIdType.id,
+        estimatedMonthlyBilling:
+          partner.legalPerson.estimatedMonthlyBilling || '',
         partnerType: 'legal',
       };
       allPartners.legalPersons.push(modeledPartner);
@@ -28,10 +81,30 @@ export const PartnersModel = ([{ data }]) => {
   }, modeledPartnersData);
 };
 
-export const PayloadLegalModel = () => {};
+export const PayloadLegalModel = data => data;
+export const PartnersLegalFormResponseModel = data => data;
 
-export const PartnersLegalFormResponseModel = () => {};
+export const PayloadNaturalModel = data => ({
+  ownershipPercentage: data.ownershipPercentage,
+  taxId: data.taxId,
+  name: data.name,
+  birthdate: data.birthDate,
+  birthPlace: data.birthPlace,
+  birthCountryid: data.birthCountryid,
+  fatherName: data.fatherName,
+  motherName: data.motherName,
+  spouseTaxId: data.spouseName,
+  spouseTaxIdType: data.spouseTaxIdType,
+  spouseName: data.spouseName,
+  additionalDocuments: [
+    {
+      documentTypeId: data.additionalDocumentId,
+      documentIdentifier: data.additionalDocumentIdentifier,
+      issuedBy: data.additionalDocumentIssuedBy,
+      issueDate: data.additionalDocumentIssueDate,
+    },
+  ],
+});
+export const PartnersNaturalFormResponseModel = data => data;
 
-export const PartnersNaturalFormResponseModel = () => {};
-
-export const PayloadNaturalModel = () => {};
+export const PayloadCreatePartnerlModel = data => data;
