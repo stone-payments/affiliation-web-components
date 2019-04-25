48f29a23723f016f41190c7d1e8d3bcfb4ddbf2d
Caio Matheus Veloso Dias <cmdias@stone.com.br>
2019-04-25 10:40:16 -0300

    chore: Update MerchantPartnersState, documentTypeIds to documentTypes, additionalDocumentTypeIds to additionalDocumentTypes and remove countryCodes.

--- packages/affiliation-merchant-partners/src/state/MerchantPartnersState.js
+++ packages/affiliation-merchant-partners/src/state/MerchantPartnersState.js
@@ -1,23 +1,13 @@
-export const documentTypeIds = [
+export const documentTypes = [
   {
     id: 2,
     name: 'CPF',
   },
 ];
 
-export const additionalDocumentTypeIds = [
+export const additionalDocumentTypes = [
   {
     id: 1,
     name: 'RG - Registro Geral',
-  },
-];
-
-export const countryCodes = [
-  {
-    id: 1,
-    name: 'test1',
-  }, {
-    id: 2,
-    name: 'test2',
   },
 ];
